c5049ae6e0b6a0addd1e8f690e98140d30ebbe7a
Your Name <you@example.com>
2022-06-11 15:14:47 -0400

    another update

--- Game.js
+++ Game.js
@@ -62,6 +62,72 @@ function move_circle(o){
 
 }
 
+//Function for bird movement (I know the bird doesn't look like a bird yet lol) - mostly uses code from circle movement for now
+function move_bird(b) {
+	let speed=12;
+
+	obj=b.display;
+
+	if (right_key_pressed()){
+		obj.x-=speed;
+	}
+	if (left_key_pressed()){
+		obj.x+=speed;
+	}
+	if (up_key_pressed()){
+		obj.y+=speed;
+	}
+	if (down_key_pressed()){
+		obj.y-=speed;
+	}
+	if (obj.x<0){
+		obj.x=0;
+	}
+	if (obj.x+obj.height>900){
+		obj.x=900-obj.height;
+	}
+	if (obj.y<0){
+		obj.y=0;
+	}
+	if (obj.y+obj.height>600){
+		obj.y=600-obj.height;
+	}
+	block=b.fields["Obstacle"];
+	position=detectCollision(o,block);
+	if(position["right"]==0 && position["left"]==-1 && Math.abs(position["top"]+position["bottom"])<2){
+		obj.x=block.display.x-obj.width;
+		position=detectCollision(o,block);
+	}
+	if(position["right"]==1 && position["left"]==0 && Math.abs(position["top"]+position["bottom"])<2){
+		obj.x=block.display.x+block.display.width;
+		position=detectCollision(o,block);
+	}
+	if(position["bottom"]==0 && position["top"]==-1 && Math.abs(position["left"]+position["right"])<2){
+		obj.y=block.display.y-obj.width;
+		position=detectCollision(o,block);
+	}
+	if(position["bottom"]==1 && position["top"]==0 && Math.abs(position["left"]+position["right"])<2){
+		obj.y=block.display.y+block.display.height;
+	}
+}
+
+
+//Collision detection - kinda janky around corners but actual game physics will be more complex
+if(position["right"]==0 && position["left"]==-1 && Math.abs(position["top"]+position["bottom"])<2){
+	obj.x=block.display.x-obj.width;
+	position=detectCollision(o,block);
+}
+if(position["right"]==1 && position["left"]==0 && Math.abs(position["top"]+position["bottom"])<2){
+	obj.x=block.display.x+block.display.width;
+	position=detectCollision(o,block);
+}
+if(position["bottom"]==0 && position["top"]==-1 && Math.abs(position["left"]+position["right"])<2){
+	obj.y=block.display.y-obj.width;
+	position=detectCollision(o,block);
+}
+if(position["bottom"]==1 && position["top"]==0 && Math.abs(position["left"]+position["right"])<2){
+	obj.y=block.display.y+block.display.height;
+}
 
 let plat=new Block(get_platform(300,300,250,200));
 plat.initialize();
